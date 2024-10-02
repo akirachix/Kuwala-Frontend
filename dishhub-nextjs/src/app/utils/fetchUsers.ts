beb53a78d0bc4d799bb3c34e16f2173613a328f1
Zippy-Waluse <zippywaluse@gmail.com>
2024-10-02 20:56:21 +0300

    added files

--- dishhub-nextjs/src/app/utils/fetchUsers.ts
+++ dishhub-nextjs/src/app/utils/fetchUsers.ts
@@ -1,25 +1,4 @@
-// const usersApi = `${process.env.BASE_URL}/api/numberUsers`; 
 
-// export const fetchUsers = async (month?: string) => {
-//   try {
-//     let apiUrl = usersApi;
-//     if (month) {
-//       apiUrl += `?month=${month}`;
-//     }
-
-//     console.log('API URL:', apiUrl); 
-
-//     const response = await fetch(apiUrl);
-//     if (!response.ok) {
-//       throw new Error('Failed to fetch users');
-//     }
-
-//     const data = await response.json();
-//     return data;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
 
 const usersApi =' /api/numberUsers/'; 
 
