ee19e0a2964db66f0ad85e3b60b27c41a4376cd0
Zippy-Waluse <zippywaluse@gmail.com>
2024-10-04 14:34:00 +0300

    added changes

--- dishhub-nextjs/tailwind.config.ts
+++ dishhub-nextjs/tailwind.config.ts
@@ -11,26 +11,17 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
-<<<<<<< HEAD
       screens: {
         nesthub: { max: "1024px" }, 
         nesthubmax: { max: "1280px" }, 
-      },
-=======
-      screens:{
         'ipm': {'min': '1024px', 'max': '1050px'},
         'ipa':{'min':'1280px', 'max':'1300px'},
+      },
       
-        
-      }
->>>>>>> ae0a171228c7b2008e3e06941301e75e193c7e5b
     },
   },
   plugins: [],
 };
-<<<<<<< HEAD
 
 export default config;
-=======
-export default config;
->>>>>>> ae0a171228c7b2008e3e06941301e75e193c7e5b
+
